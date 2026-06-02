17eea83d2044a15b41cc65f7797e224875c3504d
jaquilyn Lacandazo <lacandazojaquilyn@gmail.com>
2026-06-03 07:02:34 +0800

    Fix web.php syntax error

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,21 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SongController; // Make sure your controller is imported!
+use App\Http\Controllers\SongController; 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// 1. This loads your pink layout when logged in!
+
 Route::get('/dashboard', function () {
     return view('songs.index'); 
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// 2. This fixes the "Route [songs.create] not defined" error!
+
 Route::middleware('auth')->group(function () {
     Route::get('/songs/create', [SongController::class, 'create'])->name('songs.create');
     Route::post('/songs', [SongController::class, 'store'])->name('songs.store');
-    // Add any edit or delete song routes down here if you have them!
 });
 
 Route::middleware('auth')->group(function () {
@@ -26,4 +25,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __important_breeze_auth_routes_here__ // Leave the require __DIR__.'/auth.php'; line at the bottom!
+
+require __DIR__.'/auth.php';
